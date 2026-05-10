1caf288fe39341bac3a504e8726611449311b3b1
Erkan <erkansoftwaredeveloper@gmail.com>
2026-05-10 20:23:13 +0200

    ADD C conditionals examples (if-else, switch)

---
+++ control-flow_2/conditionals.c
@@ -0,0 +1,44 @@
+// IF STATEMENT 
+int age = 18;
+
+if (age >= 18) {
+  printf("You are an adult");
+}
+
+
+// IF - ELSE
+int age = 16;
+
+if (age >= 18) {
+  printf("Adult");
+} else {
+  printf("Not adult");
+}
+
+
+// ELSE IF
+int score = 75;
+
+if (score >= 90) {
+  printf("A");
+} else if (score >= 70) {
+  printf("B");
+} else {
+  printf("C");
+}
+
+
+// SWITCH
+int day = 2;
+
+switch(day) {
+  case 1:
+      printf("Monday");
+      break;
+  case 2:
+      printf("Tuesday");
+      break;
+  default:
+      printf("invalid day");
+}
+
